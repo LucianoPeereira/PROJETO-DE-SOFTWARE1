3edece034696107461d0c4f0200ca04b0399b1f7
LucianoPeereira <marcolucianopereira@hotmail.com>
2019-08-06 18:15:47 -0300

    Exercicio 2.5

---
+++ Capitulo 2.5/src/edu/udc/psw/Fibonacci.java
@@ -0,0 +1,12 @@
+package edu.udc.psw;
+
+public class Fibonacci {
+
+	static long fibo(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            return fibo(n - 1) + fibo(n - 2);
+        }
+    }
+}
---
+++ Capitulo 2.5/src/edu/udc/psw/Principal.java
@@ -0,0 +1,11 @@
+package edu.udc.psw;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 30; i++) {
+			System.out.print(Fibonacci.fibo(i) + "\t");
+		}
+	}
+}
