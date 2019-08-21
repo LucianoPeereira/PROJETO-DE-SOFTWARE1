d8a8bc537cf44007d232a254a528f17661ded9d7
LucianoPeereira <marcolucianopereira@hotmail.com>
2019-08-21 16:00:12 -0300

    Exercicio 3.5

--- Capitulo 3.5/src/edu/udc/psw/Aplicação.java
+++ Capitulo 3.5/src/edu/udc/psw/Aplicação.java
@@ -4,9 +4,9 @@ public class Aplicação {
 
 	public static void main(String[] args) {
 		AumentoSalarial t = new AumentoSalarial();
-		System.out.println(t.Classifica("Bobao", 2501.00));
+		System.out.println(t.Classifica("Diretor", 6000.00));
 
-		System.out.println(t.Aumento("Bobao", 2501.00));
+		System.out.println(t.Aumento("Diretor", 6000.00));
 	}
 
 }
