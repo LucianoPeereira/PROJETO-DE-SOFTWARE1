a8ba17feb26012f1200a5b4481c3fc582fd81e98
LucianoPeereira <marcolucianopereira@hotmail.com>
2019-09-11 23:18:38 -0300

    Exercicio 16.1

---
+++ Exercicio 16.1/src/edu/udc/psw/NoLista.java
@@ -0,0 +1,20 @@
+package edu.udc.psw;
+
+public class NoLista {
+	NoLista proximo;
+	NoLista anterior;
+	Object dado;
+	
+	NoLista() { //Visibilidade apenas no pacote
+		this.proximo = null;
+		this.anterior = null;
+		this.dado = null;
+	}
+	
+	NoLista(NoLista proximo, NoLista anterior, Object obj) {
+		this.proximo = proximo;
+		this.anterior = anterior;
+		this.dado = obj;
+	}
+	
+}
