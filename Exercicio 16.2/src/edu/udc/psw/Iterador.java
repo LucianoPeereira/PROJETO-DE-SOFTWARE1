553bc4e12ba57dd0b9fbb838e1ad733e420195c6
LucianoPeereira <marcolucianopereira@hotmail.com>
2019-09-23 15:14:52 -0300

    Exercicio 16.2

---
+++ Exercicio 16.2/src/edu/udc/psw/Iterador.java
@@ -0,0 +1,7 @@
+package edu.udc.psw;
+
+public interface Iterador {
+	Object getDado();
+	Object proximo(); //Muda no para proximo
+	Object anterior(); //Muda no para anterior
+}
---
+++ Exercicio 16.2/src/edu/udc/psw/aplicação/Aplicação.java
@@ -0,0 +1,37 @@
+package edu.udc.psw.aplicação;
+
+import edu.udc.psw.Iterador;
+import edu.udc.psw.ListaEncadeada;
+import edu.udc.psw.Ponto2D;
+
+public class Aplicação {
+
+	public static void main(String[] args) {
+		ListaEncadeada lista = new ListaEncadeada();
+
+			lista.inserirInicio(new Ponto2D(1, 2));
+			lista.inserirInicio(new Ponto2D(3, 4));
+			lista.InserirMeio(new Ponto2D(5, 6), 2);
+			lista.inserirFim(new Ponto2D(7, 8));
+			lista.inserirFim(new Ponto2D(9, 10));
+			lista.inserirFim(new Ponto2D(11, 12));
+			lista.inserirFim(new Ponto2D(13, 14));
+			lista.inserirFim(new Ponto2D(15, 16));
+			iterarLista(lista);
+			
+			System.out.println("Pesquisa" + lista.pesquisar(2));
+			lista.remover(2);
+
+			System.out.println("Tamanho Lista: " + lista.getTamanho());
+			for (int i = 1; i <= lista.getTamanho(); i++) {
+				System.out.println(i + " - " + lista.pesquisar(i));
+			}
+		}
+	public static void iterarLista(ListaEncadeada lista) {
+		Iterador it = lista.getInicio();
+		Object obj;
+		for(int i = 1; 
+				(obj = it.proximo()) != null; 
+				System.out.println(i++ + " -> " +obj));
+	}
+}
