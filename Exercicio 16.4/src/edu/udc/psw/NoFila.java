0a5f70b6581aa5d5c479a0c88ecd7bdfb4fd4578
LucianoPeereira <marcolucianopereira@hotmail.com>
2019-09-24 13:50:26 -0300

    Exercicio 16.4

---
+++ Exercicio 16.4/src/edu/udc/psw/NoFila.java
@@ -0,0 +1,24 @@
+package edu.udc.psw;
+
+public class NoFila {
+	NoFila anterior;
+	Object obj;
+
+	/* Construtores */
+
+	NoFila() {
+		this.anterior = null;
+		this.obj = null;
+	}
+
+	NoFila(Object obj) {
+		this.anterior = null;
+		this.obj = obj;
+	}
+
+	NoFila(NoFila anterior, Object obj) {
+		this.anterior = anterior;
+		this.obj = obj;
+	}
+
+}
