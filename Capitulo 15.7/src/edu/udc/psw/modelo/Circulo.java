2e174c90a4ff6b9a80cd6e55ed508c98b43071bc
LucianoPeereira <marcolucianopereira@hotmail.com>
2019-11-19 15:21:12 -0200

    Exercicio 15.7

---
+++ Capitulo 15.7/src/edu/udc/psw/modelo/Circulo.java
@@ -0,0 +1,51 @@
+package edu.udc.psw.modelo;
+
+import edu.udc.psw.modelo.manipulador.ManipuladorFormaGeometrica;
+
+public class Circulo implements FormaGeometrica{
+
+	@Override
+	public Ponto2D centro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double area() {
+		//double r = Math.sqrt((x*x) + (y*y));
+
+		//double area = Math.PI * r * r;
+		return 0;
+	}
+
+	@Override
+	public double perimetro() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double base() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double altura() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ManipuladorFormaGeometrica getManipulador() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FormaGeometrica clone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
